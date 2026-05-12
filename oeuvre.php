f0400189df9188a59514e1fdc31c47eb7c9f688f
Deneboo <deneborbit@hotmail.com>
2026-05-12 10:18:59 +0200

    add select oeuvre by id

--- oeuvre.php
+++ oeuvre.php
@@ -1,27 +1,20 @@
 <?php
-    require 'header.php';
     require_once(__DIR__ . '/function.php');
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
         header('Location: index.php');
     }
-
-    $oeuvre = null;
-
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
+    // retrieve the oeuvre with the id specified in the URL
+    $oeuvreStatement = $pdo->prepare('SELECT * FROM oeuvres WHERE id = ?');
+    $oeuvreStatement->execute([$_GET['id']]);
+    $oeuvre = $oeuvreStatement->fetch();
 
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
     if(is_null($oeuvre)) {
         header('Location: index.php');
     }
+    require 'header.php';
 ?>
 
 <article id="detail-oeuvre">
